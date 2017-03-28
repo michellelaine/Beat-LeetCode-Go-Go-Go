083b4b4c90d9fa4582becc8da483f799b930fa9a
michellelaine <hongruo@gwu.edu>
2017-03-27 23:32:58 -0700

    66. Plus One

---
+++ 66. Plus One.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+       
+       int len = digits.length;
+       for(int i = len - 1; i >= 0; i--) {
+           if (digits[i] != 9){
+               digits[i]++;
+               return digits;
+           }
+            //digits[i - 1]++;
+            
+           digits[i] = 0;
+        }
+        
+       int[] newDigits = new int[len + 1];
+       newDigits[0] = 1;
+       
+       return newDigits; 
+    }
+}
