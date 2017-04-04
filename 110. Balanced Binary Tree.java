2b10c129edf0e7f22674e0ab2fd2fdbca5d2d952
michellelaine <hongruo@gwu.edu>
2017-04-04 12:50:42 -0700

    upload method 2

--- 110. Balanced Binary Tree.java
+++ 110. Balanced Binary Tree.java
@@ -1,3 +1,4 @@
+//Method 1: Time:O(n),Space: O(n)
 public boolean isBalanced(TreeNode root) {
         if(root == null) {
             return true;
@@ -18,3 +19,23 @@ public boolean isBalanced(TreeNode root) {
         // return Math.max(left, right);
         return Math.max((maxDepth(root.left) + 1), (maxDepth(root.right) + 1));
     }
+
+//Method 2: Time:O(n),Space: O(n)
+    public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+    }
+    
+    private int maxDepth(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+        int left = maxDepth(node.left);
+        int right = maxDepth(node.right);
+        
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
