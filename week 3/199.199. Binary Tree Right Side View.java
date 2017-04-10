bab1957451e5b55a801eda886262887829f1a8eb
michellelaine <hongruo@gwu.edu>
2017-04-10 16:30:58 -0700

    Update 199.199. Binary Tree Right Side View.java

--- week 3/199.199. Binary Tree Right Side View.java
+++ week 3/199.199. Binary Tree Right Side View.java
@@ -1,4 +1,38 @@
-public List<Integer> rightSideView(TreeNode root) {
+//BFS Time: O(n), Space: O()
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null) {
+            return result;
+        }
+        
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        TreeNode cur;
+        
+        queue.offerLast(root);
+        
+        while(!queue.isEmpty()) {
+           // List<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                cur = queue.pollFirst();
+                if(i == 0) {
+                    result.add(cur.val);
+                }
+                if(cur.right != null){
+                    queue.offerLast(cur.right);
+                }
+                if(cur.left != null) {
+                    queue.offerLast(cur.left);
+                }
+            }
+            
+        }
+        return result;
+    }
+    //DFS
+    //Time: O(n), Space: O(n)
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
         rightView(root, result, 0);
         return result;
@@ -19,3 +53,4 @@ public List<Integer> rightSideView(TreeNode root) {
        // rightHelper(root.left, result);
         
     }
+}
